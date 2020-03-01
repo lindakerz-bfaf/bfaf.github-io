ed08cd730b946bfa59786a23d444ca9c2d46e720
lindakerz-bfaf <60583997+lindakerz-bfaf@users.noreply.github.com>
2020-03-01 20:37:22 +0000

    Update init.js

--- js/init.js
+++ js/init.js
@@ -5,42 +5,52 @@
     $('.parallax').parallax();
     $('select').formSelect();
 
-    $('#framework-results').hide();
 
-    $('#form-submit-button').click(function() {
-      $('#framework-form').hide();
-      $('#framework-results').show();
-      $(window).scrollTop(0);
-    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-function yesnohtml(question) {
+var yes = 1;
+var no = 0;
+var always = 1;
+var often = 0.75;
+var sometimes = 0.5;
+var rarely = 0.25;
+var never = 0;
+
+function yesnohtml(question, key) {
   var html = "<h6>" + question["question"] + "</h6>";
   if(question["info"]) {
     html += "<p>" + question["info"] + "</p>";
   }
   html += "<div class='form-question-answer'>";
-  html += "<div class='col s2'> <label> <input class='with-gap' name='" + question["id"] + "' type='radio' id='yes'> <span>Yes</span> </label> </div>";
-  html += "<div class='col s2'> <label> <input class='with-gap' name='" + question["id"] + "' type='radio' id='no'> <span>No</span> </label> </div>";
+  html += "<div class='col s2'> <label> <input class='with-gap' data-id='" + key + "' name='" + question["id"] + "' type='radio' id='yes'> <span>Yes</span> </label> </div>";
+  html += "<div class='col s2'> <label> <input class='with-gap' data-id='" + key + "' name='" + question["id"] + "' type='radio' id='no'> <span>No</span> </label> </div>";
   html += "</div>";
   return html;
 }
 
-function scalehtml(question) {
+function scalehtml(question, key) {
   var html = "<h6>" + question["question"] + "</h6>";
   if(question["info"]) {
     html += "<p>" + question["info"] + "</p>";
   }
   html += "<div class='form-question-answer'>";
-  html += "<div class='col s2'> <label> <input class='with-gap' name='" + question["id"] + "' type='radio' id='never'> <span>Never</span> </label> </div>";
-  html += "<div class='col s2'> <label> <input class='with-gap' name='" + question["id"] + "' type='radio' id='rarely'> <span>Rarely</span> </label> </div>";
-  html += "<div class='col s2'> <label> <input class='with-gap' name='" + question["id"] + "' type='radio' id='sometimes'> <span>Sometimes</span> </label> </div>";
-  html += "<div class='col s2'> <label> <input class='with-gap' name='" + question["id"] + "' type='radio' id='often'> <span>Often</span> </label> </div>";
-  html += "<div class='col s2'> <label> <input class='with-gap' name='" + question["id"] + "' type='radio' id='always'> <span>Always</span> </label> </div>";
+  html += "<div class='col s2'> <label> <input class='with-gap' data-id='" + key + "' name='" + question["id"] + "' type='radio' id='never'> <span>Never</span> </label> </div>";
+  html += "<div class='col s2'> <label> <input class='with-gap' data-id='" + key + "' name='" + question["id"] + "' type='radio' id='rarely'> <span>Rarely</span> </label> </div>";
+  html += "<div class='col s2'> <label> <input class='with-gap' data-id='" + key + "' name='" + question["id"] + "' type='radio' id='sometimes'> <span>Sometimes</span> </label> </div>";
+  html += "<div class='col s2'> <label> <input class='with-gap' data-id='" + key + "' name='" + question["id"] + "' type='radio' id='often'> <span>Often</span> </label> </div>";
+  html += "<div class='col s2'> <label> <input class='with-gap' data-id='" + key + "' name='" + question["id"] + "' type='radio' id='always'> <span>Always</span> </label> </div>";
   html += "</div>";
   return html;
+}
+
+function calculateResults(questions) {
+  $("form input:selected").each(function(key, response) {
+    if(key == response.attr("data-id")) {
+      console.log(response);
+    }
+  });
 }
 
 $( document ).ready(function() {
@@ -50,47 +60,58 @@ $( document ).ready(function() {
 
       $.each(questions, function(key, question) {
         if(question["type"] == "yesno") {
-          $("form").append(yesnohtml(question));
+          $("form").append(yesnohtml(question, key));
         } else if(question["type"] == "scale") {
-          $("form").append(scalehtml(question));
+          $("form").append(scalehtml(question, key));
         }
       });
       $("form").append($('#form-submit-button').parent());
     });
+
   }
+
+  $('#framework-results').hide();
+
+  $('#form-submit-button').click(function() {
+    //TODO Validation
+    $('#framework-form').hide();
+    $('#framework-results').show();
+    calculateResults(questions);
+    $(window).scrollTop(0);
+  });
 
 
 });
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
-			type: 'radar',
-			data: {
-				labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5', 'Category 6', 'Category 7'],
-				datasets: [{
-					backgroundColor: '#000000',
-					borderColor: '#000000',
-					pointBackgroundColor: '#000000',
-					data: [
-						80,
-						10,
-						30,
-						20,
-						50,
-						90,
-						5
-					]
-				},
-      ] },
-			options: {
-				scale: {
-					ticks: {
-						beginAtZero: true,
-            suggestedMax: 100
-					}
-				},
-        legend: {
-            display: false
-        }
-			}
-		});
+  type: 'radar',
+  data: {
+    labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5', 'Category 6', 'Category 7'],
+    datasets: [{
+      backgroundColor: '#000000',
+      borderColor: '#000000',
+      pointBackgroundColor: '#000000',
+      data: [
+        80,
+        10,
+        30,
+        20,
+        50,
+        90,
+        5
+      ]
+    },
+  ] },
+  options: {
+    scale: {
+      ticks: {
+        beginAtZero: true,
+        suggestedMax: 100
+      }
+    },
+    legend: {
+      display: false
+    }
+  }
+});
