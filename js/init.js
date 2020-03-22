fc762cc7706c0618c4f4e94f6ec14431ee94870a
lindakerz-bfaf <60583997+lindakerz-bfaf@users.noreply.github.com>
2020-03-22 20:36:45 +0000

    Convert questions to be multi page

--- js/init.js
+++ js/init.js
@@ -19,6 +19,16 @@ var rarely = 0.25;
 var never = 0;
 
 var SCALE_MODE = 5
+var SORTED_STAGES = ['0-2','3-10','11-20']
+var PAGES = [
+  {stage:'0-2', type: 'Symptom'},
+  {stage:'3-10', type: 'Symptom', showIf: function(results){
+    // todo
+    return true
+  }},
+  {type: 'Factor'}
+]
+var currentPage = -1
 
 function yesnohtml(question, key) {
   var html = "<h6>" + question["question"] + "</h6>";
@@ -57,36 +67,6 @@ function getTotalWeight(stage, questions){
   return totalWeight
 }
 
-function calculateResults(questions) {
-  var responses = []
-  $("form input:checked").each(function(key, response) {
-    if(key == $(response).attr("data-id")) {
-      var responseText = $(response).attr("value");
-      var responseValue = getResponseValue(responseText);
-      var question = questions[key]
-      $.each(question.clusters, function(clusterIndex, cluster){
-        var totalStageWeight = getTotalWeight(cluster.stage, questions) || 1
-        var totalWeight = getTotalWeight(null, questions) || 1
-        var weightedValue = cluster.weighting*responseValue
-        responses.push({
-            type: question.type,
-            domain: question.domain,
-            subDomain: question.subDomain,
-            factor: question.factor,
-            stage: cluster.stage,
-            response: responseText,
-            value: responseValue,
-            weightedValue: {
-              stage: weightedValue/totalStageWeight,
-              overall: weightedValue/totalWeight
-            }
-        })
-      })
-    }
-  });
-  return responses;
-}
-
 function getResponseValue(responseText) {
   if(responseText == "yes") {
     return yes;
@@ -117,7 +97,6 @@ function displayResults(results) {
     categories[type][domainKey][factor] = categories[type][domainKey][factor] || {}
     categories[type][domainKey][factor][stage] = result.weightedValue.stage
   });
-  var stages = ['0-2','3-10','11-20','>20','all']
   console.log(categories);
   var html = ''
   var tableHtml = '<table>'
@@ -125,7 +104,7 @@ function displayResults(results) {
   tableHtml += '<th>Type</th>'
   tableHtml += '<th>Domain</th>'
   tableHtml += '<th>Factor</th>'
-  $.each(stages, function(index, stage){
+  $.each(SORTED_STAGES, function(index, stage){
     tableHtml += '<th>' + stage + ' Years</th>'
   })
   tableHtml += '</tr>'
@@ -138,7 +117,7 @@ function displayResults(results) {
         tableHtml += '<td>' + type + '</td>'
         tableHtml += '<td>' + domainKey + '</td>'
         tableHtml += '<td>' + factor + '</td>'
-        $.each(stages, function(index, stage){
+        $.each(SORTED_STAGES, function(index, stage){
           if(factorCats[stage] != null){
             var value = factorCats[stage]
             tableHtml += '<td>' + numeral(value).format('0.00%') + '</td>'
@@ -159,7 +138,31 @@ function displayResults(results) {
   return categories;
 }
 
-function autoPopulateForm(questions) {
+function filterQuestions(questions, type, stage){
+  var rtnQuestions = []
+  $.each(questions, function(key, question) {
+    if(type && question.type !== type){ return }
+    if(stage){
+      var earliestStage = SORTED_STAGES.length - 1
+      $.each(question.clusters || [], function(index, cluster){
+        var stageIndex = -1
+        if(cluster.stage){
+          stageIndex = SORTED_STAGES.indexOf(cluster.stage)
+        }
+        if(stageIndex >= 0 && stageIndex < earliestStage ){
+          earliestStage = stageIndex
+        }
+      })
+      if(SORTED_STAGES[earliestStage] !== stage){
+        return
+      }
+    }
+    rtnQuestions.push(question)
+  })
+  return rtnQuestions
+}
+
+function autoPopulateForm(questions, type, stage) {
   //used for testing
   $.each(questions, function(key, question) {
     var min = 0
@@ -192,58 +195,114 @@ $( document ).ready(function() {
 
 });
 
-function onJSONLoaded(data){
-  var questions = data["questions"];
+function getNextPage(results){
+  var page = PAGES[++currentPage]
+  while(page && page.showIf != null && !page.showIf(results)){
+    page = PAGES[++currentPage]
+  }
+  return page
+}
 
+function renderPage(questions){
+  $("#form-content").empty()
   $.each(questions, function(index, question) {
     question.id = 'q' + index
     if(question["questionType"] == "yesno") {
-      $("form").append(yesnohtml(question, index));
+      $("#form-content").append(yesnohtml(question, index));
     } else if(question["questionType"] == "scale") {
-      $("form").append(scalehtml(question, index));
+      $("#form-content").append(scalehtml(question, index));
     }
   });
-  $("form").append($('#form-submit-button').parent());
-  $('#form-submit-button').click(function() {
+}
+
+function onJSONLoaded(data){
+  var questions = data.questions;
+  var page = getNextPage()
+  var pageQns = []
+  if(page){
+    pageQns = filterQuestions(questions, page.type, page.stage)
+    renderPage(pageQns)
+  }
+  var results = []
+  $('#form-submit-button').on('click', function() {
     //TODO Validation
-    $('#framework-form').hide();
-    $('#framework-results').show();
-    var results = calculateResults(questions);
-    displayResults(results);
+    results = [].concat(results, calculateResults(pageQns));
+    var page = getNextPage(results)
+    if(page){
+      pageQns = filterQuestions(questions, page.type, page.stage)
+      renderPage(pageQns)
+    } else {
+      $('#framework-form').hide();
+      $('#framework-results').show();
+      displayResults(results);
+    }
     $(window).scrollTop(0);
   });
   $("#auto-populate").on("click", function(){ autoPopulateForm(questions) });
 }
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-  type: 'radar',
-  data: {
-    labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5', 'Category 6', 'Category 7'],
-    datasets: [{
-      backgroundColor: '#000000',
-      borderColor: '#000000',
-      pointBackgroundColor: '#000000',
-      data: [
-        80,
-        10,
-        30,
-        20,
-        50,
-        90,
-        5
-      ]
-    },
-  ] },
-  options: {
-    scale: {
-      ticks: {
-        beginAtZero: true,
-        suggestedMax: 100
-      }
-    },
-    legend: {
-      display: false
+function calculateResults(questions) {
+  var responses = []
+  $("form input:checked").each(function(key, response) {
+    if(key == $(response).attr("data-id")) {
+      var responseText = $(response).attr("value");
+      var responseValue = getResponseValue(responseText);
+      var question = questions[key]
+      $.each(question.clusters, function(clusterIndex, cluster){
+        var totalStageWeight = getTotalWeight(cluster.stage, questions) || 1
+        var totalWeight = getTotalWeight(null, questions) || 1
+        var weightedValue = cluster.weighting*responseValue
+        responses.push({
+            code: question.code,
+            type: question.type,
+            domain: question.domain,
+            subDomain: question.subDomain,
+            factor: question.factor,
+            stage: cluster.stage,
+            response: responseText,
+            value: responseValue,
+            weightedValue: {
+              stage: weightedValue/totalStageWeight,
+              overall: weightedValue/totalWeight
+            }
+        })
+      })
     }
-  }
-});
+  });
+  return responses;
+}
+
+function renderChart(){
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var chart = new Chart(ctx, {
+    type: 'radar',
+    data: {
+      labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5', 'Category 6', 'Category 7'],
+      datasets: [{
+        backgroundColor: '#000000',
+        borderColor: '#000000',
+        pointBackgroundColor: '#000000',
+        data: [
+          80,
+          10,
+          30,
+          20,
+          50,
+          90,
+          5
+        ]
+      },
+    ] },
+    options: {
+      scale: {
+        ticks: {
+          beginAtZero: true,
+          suggestedMax: 100
+        }
+      },
+      legend: {
+        display: false
+      }
+    }
+  });
+}
